9f06a00f17337fc1ce9e6bed2608b1001a9e6a28
HarryCandlish <hcnd90@gmail.com>
2019-05-21 13:27:45 +1200

    finished home-page design

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,32 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+import { Link } from "gatsby"
+
+import homeStyles from "./index.module.scss"
+
+const homePage = () => {
+  return (
+    <div>
+      <div className={homeStyles.container}>
+        <h2 className={homeStyles.mainTitle}>BLOG</h2>
+        <h3 className={homeStyles.secondaryTitle}>HARRY CANDLISH</h3>
+      </div>
+      <div>
+        <ol className={homeStyles.titles}>
+          <Link className={homeStyles.title}>
+            <li className={homeStyles.list}>
+              <h2>PHILOSOPHY</h2>
+            </li>
+          </Link>
+          <Link className={homeStyles.title}>
+            <li className={homeStyles.list}>
+              <h2>TECHNOLOGY</h2>
+            </li>
+          </Link>
+        </ol>
+      </div>
+    </div>
+  )
+}
+
+export default homePage
